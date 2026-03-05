ec6f2e2a5e1b777d39998e22678445fe49de1da7
Start-bd <startbdhub@gmail.com>
2026-03-06 04:37:58 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '2mb',
+        },
+    },
+    images: {
+        domains: ['supabase.co', 'avatars.githubusercontent.com'],
+    },
+}
+
+module.exports = nextConfig
